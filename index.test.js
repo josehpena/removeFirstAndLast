e0567ce9da2dfc2cf1b4b783bad421cf0cc22e72
Jose Pena <jose.pena@ibm.com>
2023-05-04 10:21:27 -0300

    Should return the number '2' when '123' as sended

--- index.test.js
+++ index.test.js
@@ -3,4 +3,12 @@ describe('removeFirstAndLast', () => {
    it('Should return null when a unique character is sended.',() => {
         expect(removeFaL('1')).toBeNull();
    })
+
+   it('Should return null when two characters are sended.', () => {
+        expect(removeFaL('12')).toBeNull();
+   });
+
+   it(' Should return the number "2" when "123" as sended ', () => {
+        expect(removeFaL('123')).toBe("2");
+   });
 });
--- notes.md
+++ notes.md
@@ -1,3 +1,5 @@
 # Should return null when a unique character are sended. OK
 
-# Should return null when two characters are sended.
+# Should return null when two characters are sended. OK
+
+# Should return the number '2' when '123' as sended. OK
